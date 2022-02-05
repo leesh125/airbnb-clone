6d7e72d2a2e6c1dc5285469c91187bb6b2e735cc
leesh125 <chelsea12345@naver.com>
2022-02-05 17:52:51 +0900

    Review 모델 생성(format을 사용해 Review 객체 표시)

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
+from tkinter import CASCADE
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+
+    """ Review Model definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=CASCADE)
+
+    def __str__(self):
+        # Review 객체를 admin 패널에서 "review - Room(객실이름)" 이렇게 표시 가능함
+        return f"{self.review} - {self.room}"
