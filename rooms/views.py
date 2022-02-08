b0616478d43cdf4859bce9b514dcdb790a2bc067
leesh125 <chelsea12345@naver.com>
2022-02-09 00:24:56 +0900

    빈 페이지 요청시 home으로 redirect

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from math import ceil
-from django.shortcuts import render  # HttpResponse안에 html을 넣어서 보내줄 수 있다.
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect  # HttpResponse안에 html을 넣어서 보내줄 수 있다.
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
@@ -9,14 +9,14 @@ def all_rooms(request):
     # request.Get : GET 메소드로 보내지는 요청 파라미터를 불러옴
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(
-        room_list, 10, orphans=5
-    )  # orphan(고아): 마지막 페이지에 5개이하 객체가 있으면 이전 페이지로 땡겨오기
-    rooms = paginator.page(int(page))  # get_page가 아닌 page() 사용해보기
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            context={"page": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")  # 빈 페이지가 요청되면 home으로 redirect
     # rooms = paginator.get_page(page)  # 현재 페이지에 대한 Paginator 객체 리턴
-
-    return render(
-        request,
-        "rooms/home.html",
-        context={"page": rooms},
-    )
