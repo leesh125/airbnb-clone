c5a87e26ba68bb5fb4d7cc780326207bbf8e167b
leesh125 <chelsea12345@naver.com>
2022-02-08 20:15:48 +0900

    extends를 통해 공통 html을 모든 html에 적용하고자 함

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render  # HttpResponse안에 html을 넣어서 보내줄 수 있다.
 from django.http import HttpResponse
-from datetime import datetime
+from . import models
 
 
 # 장고는 Httprequest를 파이썬 객체로 변환해 준다.
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
-    # all_rooms 이름의 템플릿을 반환해준다(ex.html)
-    return render(
-        request, "all_rooms.html", context={"now": now, "hungry": hungry}
-    )  # 템플릿에 변수들을 같이 보냄
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
