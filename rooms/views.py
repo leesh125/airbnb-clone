b9a0ab588af5a31672baec63c3bb46af93d151ca
leesh125 <chelsea12345@naver.com>
2022-02-08 23:25:46 +0900

    django로 요청 메시지로 오는 파라미터를 가져올 수 있음(page=num 파라미터로 page 기능 만들기)

--- rooms/views.py
+++ rooms/views.py
@@ -5,5 +5,10 @@ from . import models
 
 # 장고는 Httprequest를 파이썬 객체로 변환해 준다.
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    # request.Get : GET 메소드로 보내지는 요청 파라미터를 불러옴
+    page = int(request.GET.get("page", 1))  # 파라미터 값을 가져옴(기본값 0)
+    page_size = 10  # 보여질 room의 수
+    limit = page_size * page  # 해당 페이지에서 보여질 room의 마지막 수
+    offset = limit - page_size  # 해당 페이지에서 보여질 room의 첫번째 수
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
