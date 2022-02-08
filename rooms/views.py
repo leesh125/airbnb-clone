6760bef0ad9a2a388cb489398133290868e0fbe8
leesh125 <chelsea12345@naver.com>
2022-02-09 00:01:46 +0900

    django paginator로 현재 파라미터로 들어온 페이지 수를 읽어서 그에 해당하는 room객체들을 가져옴

--- rooms/views.py
+++ rooms/views.py
@@ -1,28 +1,18 @@
 from math import ceil
 from django.shortcuts import render  # HttpResponse안에 html을 넣어서 보내줄 수 있다.
-from django.http import HttpResponse
+from django.core.paginator import Paginator
 from . import models
 
 
 # 장고는 Httprequest를 파이썬 객체로 변환해 준다.
 def all_rooms(request):
     # request.Get : GET 메소드로 보내지는 요청 파라미터를 불러옴
-    page = int(request.GET.get("page", 1))  # 파라미터 값을 가져옴(기본값 0)
-    page = int(page or 1)  # 잘못된 파라미터가 올 경우 1로 대체
-    page_size = 10  # 보여질 room의 수
-    limit = page_size * page  # 해당 페이지에서 보여질 room의 마지막 수
-    offset = limit - page_size  # 해당 페이지에서 보여질 room의 첫번째 수
-    page_count = ceil(
-        models.Room.objects.count() / page_size
-    )  # 전체 페이지수(실수여서 올림해서 보내야함)
-    all_rooms = models.Room.objects.all()[offset:limit]
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)  # room 객체들을 10개씩 끊음
+    rooms = paginator.get_page(page)  # 해당 페이지 파라미터에 해당되는 room들을 가져옴
     return render(
         request,
         "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),  # html에선 파이썬 코드가 안먹힘(range)
-        },
+        context={"rooms": rooms},
     )
