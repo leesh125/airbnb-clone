0d653e9743983b89fb47296a1a920d38dc05f5dc
leesh125 <chelsea12345@naver.com>
2022-02-09 00:18:52 +0900

    orphan으로 화면에 5개이하의 객체가 표시되면 이전 페이지로 끌어오기, page()사용

--- rooms/views.py
+++ rooms/views.py
@@ -7,12 +7,16 @@ from . import models
 # 장고는 Httprequest를 파이썬 객체로 변환해 준다.
 def all_rooms(request):
     # request.Get : GET 메소드로 보내지는 요청 파라미터를 불러옴
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)  # room 객체들을 10개씩 끊음
-    rooms = paginator.get_page(page)  # 해당 페이지 파라미터에 해당되는 room들을 가져옴
+    paginator = Paginator(
+        room_list, 10, orphans=5
+    )  # orphan(고아): 마지막 페이지에 5개이하 객체가 있으면 이전 페이지로 땡겨오기
+    rooms = paginator.page(int(page))  # get_page가 아닌 page() 사용해보기
+    # rooms = paginator.get_page(page)  # 현재 페이지에 대한 Paginator 객체 리턴
+
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": rooms},
+        context={"page": rooms},
     )
