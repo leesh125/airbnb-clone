4808c03f5ce0c86e20bf2c87ba9e5ed233a5493f
leesh125 <chelsea12345@naver.com>
2022-02-10 19:01:15 +0900

    CBV로 변경, 필터 똑같이 적용(하지만 객체로 넘어오기에 pk등 처리를 객체자체로 해야함)

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
-from django_countries import countries
 from . import models, forms
 
 
@@ -34,9 +33,74 @@ class RoomDetail(DetailView):  # DetailView는 기본적으로 url argument로 p
 #     except models.Room.DoesNotExist:  # 매개변수로 넘어온 pk에 해당되는 room 객체가 없으면
 #         raise Http404()  # 404 page 띄우기
 
+class SearchView(View):
 
-def search(request):
 
-    form = forms.SearchForm()  # 장고가 제공하는 form api 이용
+    """ SearchView Definition """
 
-    return render(request, "rooms/search.html", {"form": form})
+    def get(self,request):
+
+        country = request.GET.get("counrty") # country가 빈 값으로 올 경우
+
+        if country:
+            form = forms.SearchForm(request.GET)  # request 파라미터를 가져오고 기억할 수 있음
+
+            if form.is_valid(): # form 이 에러가 없으면 True 리턴
+                # cleaned_data: 선택한 값들을 가져옴(객체는 pk가 아닌 객체 그대로 가져옴)
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+
+                    if city != "Anywhere":
+                        filter_args["city__startswith"] = city
+
+                    filter_args["country"] = country
+
+                    if room_type is not None:
+                        filter_args["room_type"] = room_type
+
+                    if price is not None:
+                        filter_args["price__lte"] = price
+
+                    if guests is not None:
+                        filter_args["guests__gte"] = guests
+
+                    if bedrooms is not None:
+                        filter_args["bedrooms__gte"] = bedrooms
+
+                    if beds is not None:
+                        filter_args["beds__gte"] = beds
+
+                    if baths is not None:
+                        filter_args["baths__gte"] = baths
+
+                    if instant_book is True:
+                        filter_args["instant_book"] = True
+
+                    if superhost is True:
+                        filter_args["host__superhost"] = True
+
+                    for amenity in amenities:
+                        filter_args["amenities"] = amenity
+
+                    for facility in facilities:
+                        filter_args["facilities"] = facility
+
+                    rooms = models.Room.objects.filter(**filter_args)
+
+
+            else: # country가 빈 값으로 올 경우
+                form = forms.SearchForm() 
+
+            return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
