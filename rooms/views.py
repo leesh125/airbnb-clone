1e883cf29e56e40d4ffdf5dd4d4f8f5a58f131c7
leesh125 <chelsea12345@naver.com>
2022-02-10 19:13:28 +0900

    render 처리

--- rooms/views.py
+++ rooms/views.py
@@ -33,19 +33,19 @@ class RoomDetail(DetailView):  # DetailView는 기본적으로 url argument로 p
 #     except models.Room.DoesNotExist:  # 매개변수로 넘어온 pk에 해당되는 room 객체가 없으면
 #         raise Http404()  # 404 page 띄우기
 
-class SearchView(View):
 
+class SearchView(View):
 
     """ SearchView Definition """
 
-    def get(self,request):
+    def get(self, request):
 
-        country = request.GET.get("counrty") # country가 빈 값으로 올 경우
+        country = request.GET.get("country")  # country가 빈 값으로 올 경우
 
         if country:
             form = forms.SearchForm(request.GET)  # request 파라미터를 가져오고 기억할 수 있음
 
-            if form.is_valid(): # form 이 에러가 없으면 True 리턴
+            if form.is_valid():  # form 이 에러가 없으면 True 리턴
                 # cleaned_data: 선택한 값들을 가져옴(객체는 pk가 아닌 객체 그대로 가져옴)
                 city = form.cleaned_data.get("city")
                 country = form.cleaned_data.get("country")
@@ -62,45 +62,45 @@ class SearchView(View):
 
                 filter_args = {}
 
-                    if city != "Anywhere":
-                        filter_args["city__startswith"] = city
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
 
-                    filter_args["country"] = country
+                filter_args["country"] = country
 
-                    if room_type is not None:
-                        filter_args["room_type"] = room_type
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
 
-                    if price is not None:
-                        filter_args["price__lte"] = price
+                if price is not None:
+                    filter_args["price__lte"] = price
 
-                    if guests is not None:
-                        filter_args["guests__gte"] = guests
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
 
-                    if bedrooms is not None:
-                        filter_args["bedrooms__gte"] = bedrooms
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
 
-                    if beds is not None:
-                        filter_args["beds__gte"] = beds
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
 
-                    if baths is not None:
-                        filter_args["baths__gte"] = baths
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
 
-                    if instant_book is True:
-                        filter_args["instant_book"] = True
+                if instant_book is True:
+                    filter_args["instant_book"] = True
 
-                    if superhost is True:
-                        filter_args["host__superhost"] = True
+                if superhost is True:
+                    filter_args["host__superhost"] = True
 
-                    for amenity in amenities:
-                        filter_args["amenities"] = amenity
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
 
-                    for facility in facilities:
-                        filter_args["facilities"] = facility
+                for facility in facilities:
+                    filter_args["facilities"] = facility
 
-                    rooms = models.Room.objects.filter(**filter_args)
-
-
-            else: # country가 빈 값으로 올 경우
-                form = forms.SearchForm() 
-
-            return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+                rooms = models.Room.objects.filter(**filter_args)
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
+        else:  # country가 빈 값으로 올 경우
+            form = forms.SearchForm()
+            return render(request, "rooms/search.html", {"form": form})
