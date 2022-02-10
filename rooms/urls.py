a291f406c28ccb9eb55e1bb79d55109bf83a62af
leesh125 <chelsea12345@naver.com>
2022-02-10 16:16:35 +0900

    CBV를 통해 DetailView를 상속받아 간결하게 작성(DetailView는 기본적으로 pk를 받아오고, model만 알려주면 view에 알맞게 객체를 넘겨줌, 404page도 알아서 띄움)

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,4 +6,4 @@ from . import views
 app_name = "rooms"
 
 # rooms/ 하위 url 경로에 int값이 오면(pk 변수에 대입) views.room_detail의 요청 파라미터로 pk를 넘겨줌
-urlpatterns = [path("<int:pk>", views.room_detail, name="detail")]
+urlpatterns = [path("<int:pk>", views.RoomDetail.as_view(), name="detail")]
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.http import Http404
-from django.urls import reverse
-from django.shortcuts import redirect, render
 from . import models
 
 
@@ -17,9 +15,20 @@ class HomeView(ListView):
     context_object_name = "rooms"  # 불러오는 model object의 이름 설정
 
 
-def room_detail(request, pk):  # 요청 파라미터로부터 온 pk도 같이 넘겨받음
-    try:
-        room = models.Room.objects.get(pk=pk)  # 매개변수로 넘어온 pk의 room 객체 가져오기
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:  # 매개변수로 넘어온 pk에 해당되는 room 객체가 없으면
-        raise Http404()  # 404 page 띄우기
+# Class Based View
+class RoomDetail(DetailView):  # DetailView는 기본적으로 url argument로 pk를 찾음
+
+    """ RoomDetail Definition """
+
+    # view 한테 무슨 model을 원하는지 알려줘야함
+    model = models.Room
+    pk_url_kwargs = "potato"  # 지정한 이름으로 넘어올 매개변수 찾음
+
+
+# Function Based View
+# def room_detail(request, pk):  # 요청 파라미터로부터 온 pk도 같이 넘겨받음
+#     try:
+#         room = models.Room.objects.get(pk=pk)  # 매개변수로 넘어온 pk의 room 객체 가져오기
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:  # 매개변수로 넘어온 pk에 해당되는 room 객체가 없으면
+#         raise Http404()  # 404 page 띄우기
