c3761c71ecd5961c67e9766893e75cd161102ae2
leesh125 <chelsea12345@naver.com>
2022-02-11 11:24:23 +0900

    CSRF 공격을 막기위해, POST 메소드 시 token 생성

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.views import View
 from django.shortcuts import render
+from . import forms
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "users/login.html")
+        form = forms.LoginForm()  # form 추가 후 view에 넘겨주기
+        return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
