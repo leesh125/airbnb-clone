28a92cd3f81b5b4887eee2daed46cdb2b7d65d5b
leesh125 <chelsea12345@naver.com>
2022-02-11 12:57:51 +0900

    FormView로 간결하게 login,out 구현,reverse를 사용하면 자동으로 home을 호출하는데 url은 아직 불려지지 않음, reverse_lazy 사용을 통해 home view가 필요할 때 호출

--- users/views.py
+++ users/views.py
@@ -1,15 +1,29 @@
 from django.views import View
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm()  # form 추가 후 view에 넘겨주기
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():  # 유효한 email과 password가 들어왔으면
-            print(form.cleaned_data)
-        return render(request, "users/login.html", {"form": form})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    # url을 통해 클래스(뷰)를 불러올때 아직 url은 불려지지 않음
+    # reverse_lazy를 통해 이미 요청 응답이 끝난 페이지에서 다른 페이지로 가고자 할 때
+    # 뒤늦게 호출할 수 있다.(로그인 버튼을 누르면 동일한 url로 POST방식으로 전달 후 홈으로 REDIRECT)
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
