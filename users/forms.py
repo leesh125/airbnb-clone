7ef2e69e753ab3fefa5706e81960292c90ee11d8
leesh125 <chelsea12345@naver.com>
2022-02-11 11:38:24 +0900

    POST로 온 이메일 값을 db에 있는 값과 비교해서 없으면 에러 띄우기

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,21 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # clean_data : 모든 field를 정리해준 것에 대한 결과이다.
+    # clean_data() 의 경우 return 값을 지정하지 않으면 field를 지워버린다
+    def clean_email(self):
+        email = self.cleaned_data.get("email")  # email 필드 parameter를 가져옴
+        try:
+            models.User.objects.get(username=email)  # db에서 찾기
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        print("clean email")
--- users/views.py
+++ users/views.py
@@ -10,3 +10,6 @@ class LoginView(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
+        if form.is_valid():  # 유효한 email과 password가 들어왔으면
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
