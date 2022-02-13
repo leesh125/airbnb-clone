bc1ca2ea4fc9fedeef0537f09c6165c44c1b8179
leesh125 <chelsea12345@naver.com>
2022-02-14 00:11:00 +0900

    kakao 로그인 구현

--- users/views.py
+++ users/views.py
@@ -135,3 +135,27 @@ def github_callback(request):
         # send error message
         print("!@#!@#!")
         return redirect(reverse("users:login"))
+
+
+# kakao 로그인 요청
+def kakao_login(request):
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    )
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}"
+        )
+    except KakaoException:
+        return redirect(reverse("users:login"))
