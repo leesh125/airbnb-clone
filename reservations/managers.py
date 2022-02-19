072536cea2266e09f384de2b71f642a74ed59f43
leesh125 <chelsea12345@naver.com>
2022-02-20 01:08:56 +0900

    예약 생성과 custom managers 작성(db 접근 시에 값이 없을때 error가 아닌 None으로)

---
+++ reservations/managers.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# custom db 조작
+class CustomReservationManager(models.Manager):
+    # 찾고자 하는 값이 없을때 error가 아닌 None
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
