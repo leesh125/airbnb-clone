99bc7029cea30b488f4844aa354b6c07ca9fcd9b
leesh125 <chelsea12345@naver.com>
2022-02-20 18:17:11 +0900

    자동 번역 기능(request header에서 lang 얻기)

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views import View
 from django.views.generic import FormView, DetailView, UpdateView
@@ -292,3 +293,10 @@ def switch_hosting(request):
     except KeyError:
         request.session["is_hosting"] = True  # 세션 없으면 추가
     return redirect(reverse("core:home"))
+
+
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        pass
+    return HttpResponse(status=200)
