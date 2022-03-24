34d34224eb5273af01387e875e36d17a32244dd7
leesh125 <chelsea12345@naver.com>
2022-03-24 18:59:25 +0900

    debug false

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "_8(i*q-^)v)%5+wi(*d8#l^cr@(=aujx-%99scw#n4$3$x4@0v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = "*"
 
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if DEBUG:
+if DEBUG is False:
 
     DATABASES = {
         "default": {
