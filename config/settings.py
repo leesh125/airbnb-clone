9208b1c96ec97d90eee636d22464c96855572e91
leesh125 <chelsea12345@naver.com>
2022-03-30 02:44:55 +0900

    postgresql11

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if DEBUG if False:
+if DEBUG is False:
 
     DATABASES = {
         "default": {
