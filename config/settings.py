e8b64aeb06e3b92d4e7ccf0e8f4324fec2b6a054
leesh125 <chelsea12345@naver.com>
2022-03-30 02:30:30 +0900

    postgresql9

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ if DEBUG:
 
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
+            "ENGINE": "django.db.backends.sqlite33",
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
