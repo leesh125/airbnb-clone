55bfeb72579c0933582f479aeadfeabaf8e04563
leesh125 <chelsea12345@naver.com>
2022-03-30 03:42:22 +0900

    rds

--- config/settings.py
+++ config/settings.py
@@ -105,9 +105,9 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
+            "HOST": "airbnb-clone.cgnr1v7oo4ez.ap-northeast-2.rds.amazonaws.com",
+            "NAME": "postgres",
+            "USER": "postgres",
             "PASSWORD": os.environ.get("RDS_PASSWORD"),
             "PORT": "5432",
         }
