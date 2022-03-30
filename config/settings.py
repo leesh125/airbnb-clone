65dbf1635d790eb4404ee54745c56bd2d7daf6b7
leesh125 <chelsea12345@naver.com>
2022-03-30 17:28:56 +0900

    seed4

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if DEBUG:
+if DEBUG is False:
 
     DATABASES = {
         "default": {
